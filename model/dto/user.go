f2c8da1261487ea6f895cc186c0e9372cf85c298
Ivan <alexander.gumilang-guest@moladin.com>
2025-09-28 01:35:38 +0700

    feat: update role validation in RegisterRequest to include 'student' and 'business'

--- model/dto/user.go
+++ model/dto/user.go
@@ -8,5 +8,5 @@ type LoginRequest struct {
 type RegisterRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8"`
-	Role     string `json:"role" binding:"required,oneof=admin user"`
+	Role     string `json:"role" binding:"required,oneof=student business admin"`
 }
